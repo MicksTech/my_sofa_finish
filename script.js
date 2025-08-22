51052b9b219e7f4e7cc62119abbff82a03616ae4
MicksTech <accpersonal371@gmail.com>
2025-08-22 13:22:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+AOS.init({
+    duration: 1000,
+    once: true
+});
+
+document.getElementById('currentYear').textContent = new Date().getFullYear();
---
+++ script.lua
@@ -0,0 +1,7 @@
+<link rel="icon" href="developer.jpg">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+body {
+    background: linear-gradient(rgba(50, 50, 50, 0.5), rgba(60, 50, 50, 0.5)), 
+    url(About.jpg) center/cover no-repeat;
+}
